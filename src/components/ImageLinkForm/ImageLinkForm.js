d7dfc2511d7b123a85a6cfacd8a071976e0a61fe
Anthony Hung Nguyen <nguyenphuchung1999@gmail.com>
2019-03-09 22:38:26 +0700

    minor changes

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,7 +7,7 @@ const ImageLinkForm = ({ onInputChange, onSubmit }) => {
 			<p className='f3'>{'This Magic Brain will detect faces in your pictures, give it a try.'}</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
-					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} placeholder='enter image url'/>
 					<button className='w-30 grow f4 link ph3 pv2 div white bg-dark-gray' onClick={onSubmit}>
 						Detect
 					</button>
