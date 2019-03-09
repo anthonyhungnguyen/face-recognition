606483d3c01e0d399e5629b8e681526b1ab04c91
Anthony Hung Nguyen <nguyenphuchung1999@gmail.com>
2019-03-09 11:40:20 +0700

    Updates

---
+++ precache-manifest.f4869026f33a95862cd8e0627b21326e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d17d9f01122d8b7be24f",
+    "url": "/face-recognition/static/css/main.b8c64653.chunk.css"
+  },
+  {
+    "revision": "d17d9f01122d8b7be24f",
+    "url": "/face-recognition/static/js/main.4181462c.chunk.js"
+  },
+  {
+    "revision": "4f80a46f991ce10a0145",
+    "url": "/face-recognition/static/js/runtime~main.7c17b3be.js"
+  },
+  {
+    "revision": "d5c58d9b90adb1c19757",
+    "url": "/face-recognition/static/css/2.c317e5b1.chunk.css"
+  },
+  {
+    "revision": "d5c58d9b90adb1c19757",
+    "url": "/face-recognition/static/js/2.f962c718.chunk.js"
+  },
+  {
+    "revision": "5e892e88fef998beb61f49a442ccb535",
+    "url": "/face-recognition/static/media/ava.5e892e88.jpg"
+  },
+  {
+    "revision": "c3071cfc5230d07806463fef60ce7cec",
+    "url": "/face-recognition/index.html"
+  }
+];
